866d7dfb9138049d12085c70f4a9ad302b84f49f
ulysses-ck <ulisesapaza.280@gmail.com>
2024-06-11 09:51:13 -0300

    chore: Update email input form in Home component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
+import { suscribeEmail } from "./actions";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center min-h-screen">
-      <form className="flex flex-col gap-2">
+      <form className="flex flex-col gap-2" action={suscribeEmail}>
         <label htmlFor="email" className="flex gap-2 items-center">
           <span>Email:</span>
           <input
             type="email"
+            name="email"
             id="email"
             autoCapitalize="off"
             autoComplete="email"
@@ -16,7 +18,9 @@ export default function Home() {
             className="focus:outline-none border border-blue-300 p-2"
           ></input>
         </label>
-        <button type="submit" className="border border-blue-800">Add contact</button>
+        <button type="submit" className="border border-blue-800">
+          Add contact
+        </button>
       </form>
     </main>
   );
