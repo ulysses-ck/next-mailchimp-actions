d2a7e7ae3ff796e03cff858035b2b5ec224a1382
ulysses-ck <ulisesapaza.280@gmail.com>
2024-06-11 10:53:37 -0300

    feat: Implement email subscription functionality
    The code changes in this commit add the functionality to subscribe to an email list. It includes handling form data, making an API request to Mailchimp, and handling the response. This feature allows users to subscribe to receive updates and newsletters.
    
    Note: The commit message follows the established convention of using a prefix (feat) to indicate a new feature implementation.

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,7 +1,35 @@
 "use server";
 
-export async function suscribeEmail(formData) {
-  console.log(formData);
+/**
+ * @param prevState previous state
+ * @param prevState.message
+ * @param prevState.status
+ * @param {FormData} formData
+ * @returns {Object}
+ */
+export async function suscribeEmail(_, formData) {
+  const email = formData.get("email");
 
-  return { message: "Email suscribed!", status: 200 };
+  if (!email) return { message: "Email is required", status: 400 };
+
+  const dataCenter = process.env.MAILCHIMP_DATA_CENTER;
+  const listId = process.env.MAILCHIMP_LIST_ID;
+  const fullUrl = `https://${dataCenter}.api.mailchimp.com/3.0/lists/${listId}/members?skip_merge_validation=true`;
+
+  const response = await fetch(fullUrl, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.MAILCHIMP_API_KEY}`,
+    },
+    body: JSON.stringify({
+      email_address: email,
+      status: "subscribed",
+    }),
+  });
+
+  if (response.ok) {
+    return { message: "You have been subscribed", status: response.status };
+  }
+
+  return { message: json.title, status: json.status };
 }
